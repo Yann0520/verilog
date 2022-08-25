0288b020f029d305f12a95af0350a10392123d35
Lloid Ian Operio <109788582+operiolloid@users.noreply.github.com>
2022-08-25 08:21:12 +0800

    Update vctr.v

--- vctr.v
+++ vctr.v
@@ -5,8 +5,12 @@ module vctr(
 	output	[7:0]	rx_data,
 	output	[7:0]	rx_data_out,
 	output	[7:0]	rx_data_in,
-	output	[7:0]	vctr_data_out
+	output	[7:0]	vctrin
 	);
+	wire rx_vctr_comp;
+	wire vctr_complete;
+	wire [7:0]	vctr_data_out;
+	wire [7:0]	vctr_data_in;
 	
 	rx RX(
 	.nrst(nrst),
@@ -14,16 +18,31 @@ module vctr(
 	.rx(rx),
 	.rx_data(rx_data),
 	.rx_data_out(rx_data_out),
-	.rx_data_in(rx_data_in)
+	.rx_data_in(rx_data_in),
+	.vctr_out(vctr_out),
+	.vctr_in(vctr_in),
+//	.vctr_complete(vctr_complete),
+	.rx_vctr_comp(rx_vctr_comp),
+	.rx_vctr_comp_out(rx_vctr_comp_out),
+	.rx_vctr_comp_in(rx_vctr_comp_in)
 	);
 	
-	vctrout VECTOR_OUT(
+	
+	vctrin VECTOR_IN(
 	.clock(clock),
 	.nrst(nrst),
+	.vctr_data_in(vctr_data_in),
+	.vctr_comp_in(vctr_comp_in),
+	.vctrin(vctrin),
+	.vctr_comp_out(vctr_comp_out),
 	.vctr_data_out(vctr_data_out)
 	);
 	
 	assign vctr_data_out = rx_data_out;
+	assign vctr_data_in = rx_data_in;
+	assign rx_vctr_comp_in = vctr_comp_in;
+	assign rx_vctr_comp_out = vctr_comp_out;
+	
 endmodule
 	
 	
